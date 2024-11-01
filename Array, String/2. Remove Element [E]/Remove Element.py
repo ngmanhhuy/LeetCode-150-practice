85ed8fd7eca988cd2d9a1e8d60eab98d93056663
Huy Nguyen <ngmanhhuy@users.noreply.github.com>
2024-11-01 12:18:11 -0700

    Remove Element

--- Array, String/2. Remove Element [E]/Remove Element.py
+++ Array, String/2. Remove Element [E]/Remove Element.py
@@ -1,3 +1,17 @@
 class Solution:
     def removeElement(self, nums, val) -> int:
-        pass
+        k = 0
+        for i in range(0, len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+
+#test
+nums = [3, 2, 2, 3]
+val = 3
+k = Solution().removeElement(nums, val)
+print(k)
+print(nums)
+
